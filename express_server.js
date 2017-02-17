888c05493b7012eae9e9490468a3ce759bf8f0f1
Benton <chan.benton+test@gmail.com>
2017-02-17 21:19:23 +0000

    EJS templating added with header-partial (title only)

--- express_server.js
+++ express_server.js
@@ -2,10 +2,15 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.set("view engine", "ejs");
+
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -21,4 +26,18 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// Lists all URLs
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { 
+    shortURL: req.params.id,
+    urls: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
 });
